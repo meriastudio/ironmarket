777409523ed3c33516089ba441514d6fe305b269
MR <109487762+meriastudio@users.noreply.github.com>
2023-06-05 15:08:06 -0400

    Fixed responsiveness and improved the quality of the logo icon.

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@
   wow = new WOW(
     {
       animateClass: 'animated',
-      offset:       100,
-      callback:     function(box) {
+      offset: 100,
+      callback: function(box) {
         console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
       }
     }
